bb33f461c332799ee5aef294f6e99b7c1f618c56
philastrio <han_gang@naver.com>
2020-02-22 10:04:56 +0900

    create Post on rest api(1)

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from "express";
 
 import { connect } from "../database";
+import { Post } from "../interface/Post";
 
 export async function getPosts(req: Request, res: Response): Promise<Response> {
   const conn = await connect();
   const posts = await conn.query("SELECT * FROM posts");
   return res.json(posts[0]);
+}
+
+export async function createPost(req: Request, res: Response) {
+  const newPost: Post = req.body;
+  console.log(newPost);
+  return res.json({
+    message: "Post Created"
+  });
 }
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 const router = Router();
 
-import { getPosts } from "../controllers/post.controller";
+import { getPosts, createPost } from "../controllers/post.controller";
 
-router.route("/").get(getPosts);
+router
+  .route("/")
+  .get(getPosts)
+  .post(createPost);
 
 export default router;
